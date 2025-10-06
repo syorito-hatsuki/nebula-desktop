9baecdf013734617f1b157436c3cea4f8acfb883
syorito-hatsuki <enderdron3@gmail.com>
2025-10-06 16:43:00 +0300

    Extracted process launching

---
+++ composeApp/src/jvmMain/kotlin/dev/syoritohatsuki/nebuladesktop/util/ProcessLauncher.kt
@@ -0,0 +1,34 @@
+package dev.syoritohatsuki.nebuladesktop.util
+
+import java.io.File
+import java.lang.ProcessBuilder
+import java.nio.file.Path
+
+object ProcessLauncher {
+    fun runNebulaWithElevation(config: Path, nebulaBinary: Path): Process {
+        val os = System.getProperty("os.name").lowercase()
+
+        val command = when {
+            os.contains("win") -> listOf(
+                "powershell", "-Command",
+                "Start-Process -Verb runAs '${nebulaBinary}' '-config ${config}'"
+            )
+
+            os.contains("mac") -> listOf(
+                "osascript", "-e",
+                "do shell script \"${nebulaBinary} -config ${config}\" with administrator privileges"
+            )
+
+            else -> when {
+                File("/usr/bin/pkexec").exists() ->
+                    listOf("pkexec", nebulaBinary.toString(), "-config", config.toString())
+                else ->
+                    listOf("sudo", nebulaBinary.toString(), "-config", config.toString())
+            }
+        }
+
+        return ProcessBuilder(command)
+            .redirectErrorStream(true)
+            .start()
+    }
+}
