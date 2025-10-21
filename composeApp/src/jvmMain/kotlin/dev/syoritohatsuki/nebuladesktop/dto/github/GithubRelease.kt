53d020db76210ae5dbc211cdcb33ec801497bc93
syorito-hatsuki <enderdron3@gmail.com>
2025-10-21 21:27:50 +0300

    Added default values for API object

--- composeApp/src/jvmMain/kotlin/dev/syoritohatsuki/nebuladesktop/dto/github/GithubRelease.kt
+++ composeApp/src/jvmMain/kotlin/dev/syoritohatsuki/nebuladesktop/dto/github/GithubRelease.kt
@@ -5,12 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class GithubRelease(
-    val assets: List<GithubReleaseAsset>,
-    @SerialName("tag_name") val tagName: String
+    val assets: List<GithubReleaseAsset> = emptyList(),
+    @SerialName("tag_name") val tagName: String = ""
 ) {
     @Serializable
     data class GithubReleaseAsset(
-        val name: String,
-        @SerialName("browser_download_url") val browserDownloadUrl: String
+        val name: String = "",
+        @SerialName("browser_download_url") val browserDownloadUrl: String = ""
     )
 }
